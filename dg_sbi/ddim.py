12d0750f32cd563f0fe2ee1e0f1d67209b5d287d
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-01-30 19:33:20 +0000

    moving to laptop

--- dg_sbi/ddim.py
+++ dg_sbi/ddim.py
@@ -1,3 +1,6 @@
+from collections.abc import Callable
+from dataclasses import dataclass
+
 import flax.linen as nn
 import jax
 import jax.numpy as jnp
@@ -5,4 +8,18 @@ import jax.random as random
 import optax
 from flax.training import train_state
 
-from collections.abc import Callable
+
+@dataclass
+class DDIM:
+    """Denoising Diffusion Implicit Model (DDIM).
+
+    https://arxiv.org/abs/2010.02502
+    """
+
+    beta: jax.Array = jnp.linspace(0.001, 0.02, 100)
+    pass
+
+    def __post_init__(self):
+        alpha = 1 - self.beta
+        alpha_bar = jnp.cumprod(alpha, 0)
+        alpha_bar = jnp.concatenate((jnp.array([1.0], alpha_bar[:-1])))
