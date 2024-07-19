e28d848da40f48d0d24bfd5b0a5d7922499b00bb
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-07-19 16:17:36 +0100

    removing and ignoring local bash scripts

--- benchmarks/meta_regression/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python celeba.py +model=sptx_full_256v4 +seed=7 +wandb=True
-python celeba.py +model=sptx_fast +seed=7 +wandb=True +name="RFF 256v4"
-python celeba.py +model=sptx_full_256v10 +seed=7 +wandb=True
