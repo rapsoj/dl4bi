14ca3b0e98c6acd4e32400298e50912dba32d0cf
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-07-26 23:45:14 +0100

    working on gp grad clipping

--- benchmarks/meta_regression/gp.py
+++ benchmarks/meta_regression/gp.py
@@ -35,10 +35,9 @@ def main(cfg: DictConfig):
     dataloader = build_gp_dataloader(cfg.exp, cfg.kernel)
     train_num_steps, valid_num_steps = 100000, 5000
     valid_interval, plot_interval = 25000, 50000
-    lr_peak, lr_pct_warmup = 5e-4, 0.3
+    lr_peak, lr_pct_warmup = 1e-3, 0.3
     lr_schedule = cosine_annealing_lr(train_num_steps, lr_peak, lr_pct_warmup)
     optimizer = optax.chain(optax.clip_by_global_norm(1.0), optax.yogi(lr_schedule))
-    # optimizer = optax.chain(optax.clip_by_global_norm(1.0), optax.yogi(lr_peak))
     model = instantiate(cfg.model)
     state = train(
         rng_train,
