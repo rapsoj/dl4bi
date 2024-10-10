78575340cd2c14a0c174cf9902e8d9193d135ae2
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-10-10 09:27:39 +0100

    tweak to bayes opt summarize

--- benchmarks/meta_regression/bayes_opt_summarize.py
+++ benchmarks/meta_regression/bayes_opt_summarize.py
@@ -18,7 +18,7 @@ def main():
     df.columns = ["gp", "kernel", "seed", "model", "regret"]
     df = df.groupby(["gp", "kernel", "model"]).agg({"regret": ["mean", "std"]})
     df.columns = ["mean", "std"]
-    df = df.apply(lambda r: f"${r['mean']:0.2f}\\pm{r['std']:.2f}$", axis=1)
+    df = df.apply(lambda r: f"${r['mean']:0.3f}\\pm{r['std']:.3f}$", axis=1)
     df = df.reset_index()
     colnames = list(df.columns)
     colnames[-1] = "regret"
