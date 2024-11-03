8ea0ea0c964e6feb8b372894baeee9fe19842b41
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-11-03 00:41:34 +0000

    updates

--- dl4bi/core/bias.py
+++ dl4bi/core/bias.py
@@ -1,8 +1,11 @@
+from collections.abc import Callable
 from typing import Optional
 
+import flax.linen as nn
 import jax
 import jax.numpy as jnp
 from jax import jit, vmap
+from sps.kernels import rbf
 
 from .utils import outer_subtract
 
@@ -29,3 +32,22 @@ def distance_sq_bias(
 ):
     d_sq = vmap(outer_subtract)(qs, ks) ** 2
     return -d_sq.sum(axis=-1)[:, None, ...]  # [B, 1, Q, K]
+
+
+# TODO(danj): support var and period?
+class KernelBias(nn.Module):
+    kernel: Callable = rbf
+    num_heads: int = 4
+
+    @nn.compact
+    def __call__(
+        self,
+        qs: jax.Array,
+        ks: jax.Array,
+        valid_lens_qs: Optional[jax.Array] = None,
+        valid_lens_ks: Optional[jax.Array] = None,
+    ):
+        var = 1.0
+        init_ones = nn.initializers.constant(1)
+        ls = self.param("ls", init_ones, (1, self.num_heads, 1, 1))
+        return vmap(rbf)(qs, ks, var, ls)
