d7e68c2753806ec3665a3a12ce989c9677651d65
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-12-11 18:10:10 +0000

    adding notes to dist

--- dl4bi/core/dist.py
+++ dl4bi/core/dist.py
@@ -1,11 +1,13 @@
 from collections.abc import Callable
 from functools import partial
 
+import cupy as cp
 import jax
 import jax.numpy as jnp
 import numpy as np
+from cupyx.scipy.spatial import KDTree as cpKDTree
 from jax import jit
-from scipy.spatial import KDTree
+from scipy.spatial import KDTree as spKDTree
 from sps.kernels import l2_dist
 
 
@@ -28,13 +30,16 @@ def scipy_k_nearest_senders(r: jax.Array, s: jax.Array, k: int):
     r"""Slower than JAX's O(n^2) implementation for small tasks, but scales in $O(N\log N)$."""
     d_shape = jax.ShapeDtypeStruct((r.shape[0], k), jnp.float32)
     idx_shape = jax.ShapeDtypeStruct((r.shape[0], k), jnp.int32)
-    f = lambda r, s, k: KDTree(np.array(s)).query(np.array(r), int(k))
+    f = lambda r, s, k: spKDTree(np.array(s)).query(np.array(r), int(k))
     d, idx = jax.pure_callback(f, (d_shape, idx_shape), r, s, k)
     return idx.flatten(), d.flatten()
 
 
-# TODO(danj): try KDTree on cuda
+# TODO(danj): try KDTree on cuda; note this isn't yet in release 13.3.0
 # https://docs.cupy.dev/en/latest/reference/generated/cupyx.scipy.spatial.KDTree.html
 # https://arxiv.org/abs/2211.00120
-def k_nearest_senders_gpu(rx: jax.Array, tx: jax.Array, k: int):
-    raise NotImplementedError()
+def cupy_k_nearest_senders(r: jax.Array, s: jax.Array, k: int):
+    r = cp.asarray(r.addressable_data(0))
+    s = cp.asarray(s.addressable_data(0))
+    d, idx = cpKDTree(s).query(r, k)
+    return jnp.array(idx).flatten(), jnp.array(d).flatten()
