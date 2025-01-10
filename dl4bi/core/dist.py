21122dbada999cd50d28efb45ef5e812a65b8eac
Daniel Jenson <daniel.a.jenson@gmail.com>
2025-01-10 15:00:16 +0000

    creating memory aware k nearest neighbors

--- dl4bi/core/dist.py
+++ dl4bi/core/dist.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 from functools import partial
+from typing import Optional
 
 import jax
 import jax.numpy as jnp
@@ -9,18 +10,37 @@ from scipy.spatial import KDTree as spKDTree
 from sps.kernels import l2_dist
 
 
-@partial(jit, static_argnames=("k", "dist"))
+@partial(jit, static_argnames=("k", "dist", "batch_size"))
 def k_nearest_senders(
     r: jax.Array,
     s: jax.Array,
     k: int,
     dist: Callable = l2_dist,
+    batch_size: Optional[int] = None,
 ):
-    r"""Faster than scipy's `KDTree` for small tasks, but uses a $O(n^2)$ memory."""
-    d = dist(r, s)
-    idx = jnp.argsort(d, axis=-1)
-    d = jnp.take_along_axis(d, idx, axis=-1)
-    return idx[:, :k].flatten(), d[:, :k].flatten()
+    r"""Parellelized brute force kNN with optional `batch_size`.
+
+    Args:
+        r: Reciever positions.
+        s: Sender positions.
+        k: Number of neighbors per receiver.
+        dist: Distance function to use.
+        batch_size: Number of receivers to run in parallel. By default,
+            the method runs all of them, i.e. is a `vmap`.
+
+    Returns:
+        Index and distance arrays, each of dimension |r| x k.
+    """
+    if batch_size is None:
+        batch_size = r.shape[0]
+
+    def process_batch(r_b: jax.Array):
+        d = dist(r_b, s)
+        idx = jnp.argsort(d, axis=-1)
+        d = jnp.take_along_axis(d, idx, axis=-1)
+        return idx[:, :k].flatten(), d[:, :k].flatten()
+
+    return jax.lax.map(process_batch, r, batch_size=batch_size)
 
 
 @partial(jit, static_argnames=("k",))
