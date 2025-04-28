273e0856bb392e50e1e4cf1b2f13bf78af44b3e4
Daniel Jenson <daniel.a.jenson@gmail.com>
2025-04-28 16:46:13 +0100

    updating te tnp and tnp d to accomodate time and fixed effects

--- dl4bi/meta_learning/te_tnp.py
+++ dl4bi/meta_learning/te_tnp.py
@@ -7,10 +7,19 @@ import jax.numpy as jnp
 from ..core.mlp import MLP
 from ..core.model_output import DiagonalMVNOutput
 from ..core.transformer import TEISTEncoder
+from ..core.utils import safe_stack
 from .steps import likelihood_train_step, likelihood_valid_step
+from .utils import first_shape
 
 
 class TETNP(nn.Module):
+    """[A Translation Equivariant Transformer Neural Process](https://arxiv.org/abs/2406.12409).
+
+    .. note::
+        By default, this uses an Induced Set Transformer (IST), which the
+        authors noted performs best when using pseudo tokens.
+    """
+
     encoder: Callable = TEISTEncoder()
     decoder: Callable = MLP([128, 128, 2])
     embed_f: Callable = MLP([128, 128])
@@ -21,18 +30,28 @@ class TETNP(nn.Module):
     @nn.compact
     def __call__(
         self,
-        s_ctx: jax.Array,  # [B, L_ctx, D_S]
-        f_ctx: jax.Array,  # [B, L_ctx, D_F]
-        s_test: jax.Array,  # [B, L_test, D_S]
+        x_ctx: Optional[jax.Array] = None,  # [B, L_ctx, D_x]
+        s_ctx: Optional[jax.Array] = None,  # [B, L_ctx, D_s]
+        t_ctx: Optional[jax.Array] = None,  # [B, L_ctx, D_t]
+        f_ctx: Optional[jax.Array] = None,  # [B, L_ctx, D_f]
         mask_ctx: Optional[jax.Array] = None,  # [B, L_ctx]
+        x_test: Optional[jax.Array] = None,  # [B, L_test, D_x]
+        s_test: Optional[jax.Array] = None,  # [B, L_test, D_s]
+        t_test: Optional[jax.Array] = None,  # [B, L_test, D_t]
         training: bool = False,
         **kwargs,
     ):
-        (B, L_ctx, D_f), L_test = f_ctx.shape, s_test.shape[1]
+        (B, L_ctx, _D_f), L_test = f_ctx.shape, s_test.shape[1]
         obs_ind, unobs_ind = jnp.ones((B, L_ctx, 1)), jnp.zeros((B, L_test, 1))
+        test_shape = first_shape([x_test, s_test, t_test])
+        f_test = jnp.zeros((*test_shape[:-1], f_ctx.shape[-1]))
         f_ctx = jnp.concat([f_ctx, obs_ind], axis=-1)
-        f_test = jnp.concat([jnp.zeros((B, L_test, D_f)), unobs_ind], axis=-1)
+        f_test = jnp.concat([f_test, unobs_ind], axis=-1)
         f_ctx_embed, f_test_embed = self.embed_f(f_ctx), self.embed_f(f_test)
+        # NOTE: All inputs other than the function value are considered part of
+        # the index set for translation equivariance (see Appendix F.4).
+        s_ctx = safe_stack(x_ctx, s_ctx, t_ctx)
+        s_test = safe_stack(x_test, s_test, t_test)
         f_test_enc = self.encoder(f_test_embed, f_ctx_embed, s_test, s_ctx, mask_ctx)
         output = self.decoder(f_test_enc)
         return self.output_fn(output)
--- dl4bi/meta_learning/tnp_d.py
+++ dl4bi/meta_learning/tnp_d.py
@@ -7,7 +7,9 @@ import jax.numpy as jnp
 from ..core.mlp import MLP
 from ..core.model_output import DiagonalMVNOutput
 from ..core.transformer import TransformerEncoder
+from ..core.utils import safe_stack
 from .steps import likelihood_train_step, likelihood_valid_step
+from .utils import first_shape
 
 
 class TNPD(nn.Module):
@@ -36,10 +38,14 @@ class TNPD(nn.Module):
     @nn.compact
     def __call__(
         self,
-        s_ctx: jax.Array,  # [B, L_ctx, D_S]
-        f_ctx: jax.Array,  # [B, L_ctx, D_F]
-        s_test: jax.Array,  # [B, L_test, D_S]
+        x_ctx: Optional[jax.Array] = None,  # [B, L_ctx, D_x]
+        s_ctx: Optional[jax.Array] = None,  # [B, L_ctx, D_s]
+        t_ctx: Optional[jax.Array] = None,  # [B, L_ctx, D_t]
+        f_ctx: Optional[jax.Array] = None,  # [B, L_ctx, D_f]
         mask_ctx: Optional[jax.Array] = None,  # [B, L_ctx]
+        x_test: Optional[jax.Array] = None,  # [B, L_test, D_x]
+        s_test: Optional[jax.Array] = None,  # [B, L_test, D_s]
+        t_test: Optional[jax.Array] = None,  # [B, L_test, D_t]
         training: bool = False,
         **kwargs,
     ):
@@ -47,28 +53,30 @@ class TNPD(nn.Module):
 
         Args:
             rng: A psuedo-random number generator.
-            s_ctx: A location array of shape `[B, L_ctx, D_S]` where
-                `B` is batch size, `L_ctx` is number of context
-                locations, and `D_S` is the dimension of each location.
-            f_ctx: A function value array of shape `[B, L_ctx, D_F]` where `B` is
-                batch size, `L_ctx` is number of context locations, and `D_F` is
-                the dimension of each function value.
-            s_test: A location array of shape `[B, L_test, D_S]` where `B` is
-                batch size, `L_test` is number of test locations, and `D_S`
-                is the dimension of each location.
-            mask_ctx: An optional array of shape `[B, L_ctx]`
+            x_ctx: Optional fixed effects for context points.
+            t_ctx: Optional temporal values for context points.
+            s_ctx: Optional spatial values for context points.
+            f_ctx: Function values for context points.
+            mask_ctx: A mask for context points.
+            x_test: Optional fixed effects for test points.
+            t_test: Optional temporal values for test points.
+            s_test: Optional spatial values for test points.
+            f_test: Function values for test points.
             training: A boolean indicating whether this call is performed during
                 training.
 
         Returns:
-            $\mu_f,\log(\sigma_f^2\in\mathbb{R}^{B\times L_\text{test}\times D_F}$.
+            `ModelOutput`.
         """
         (B, L_ctx), L_test = s_ctx[:2], s_test.shape[1]
-        s_f_ctx = jnp.concatenate([s_ctx, f_ctx], axis=-1)
-        f_test = jnp.zeros([*s_test.shape[:-1], f_ctx.shape[-1]])
-        s_f_test = jnp.concatenate([s_test, f_test], axis=-1)
-        s_f = jnp.concatenate([s_f_ctx, s_f_test], axis=1)
-        s_f_embed = self.embed_s_f(s_f, training)
+        test_shape = first_shape([x_test, s_test, t_test])
+        f_test = jnp.zeros((*test_shape[:-1], f_ctx.shape[-1]))
+        # NOTE: TNP-D does differentiate between fixed effects
+        # locations, and time, so stack them all together.
+        ctx = safe_stack(x_ctx, s_ctx, t_ctx, f_ctx)
+        test = safe_stack(x_test, s_test, t_test, f_test)
+        ctx_test = jnp.concatenate([ctx, test], axis=1)
+        ctx_test_embed = self.embed_s_f(ctx_test, training)
         if mask_ctx is None:
             mask_ctx = jnp.ones((B, L_ctx), dtype=bool)
             mask_test = jnp.zeros((B, L_test), dtype=bool)
@@ -80,7 +88,7 @@ class TNPD(nn.Module):
                 mode="constant",
                 constant_values=False,
             )
-        s_f_enc = self.enc(s_f_embed, mask, training, **kwargs)
-        s_f_test_enc = s_f_enc[:, -L_test:, ...]
-        output = self.head(s_f_test_enc, training)
+        ctx_test_enc = self.enc(ctx_test_embed, mask, training, **kwargs)
+        test_enc = ctx_test_enc[:, -L_test:, ...]
+        output = self.head(test_enc, training)
         return self.output_fn(output)
