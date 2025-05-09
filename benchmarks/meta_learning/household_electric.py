1077369ae125dd650120a38851b00bd77d8ff73a
Daniel Jenson <daniel.a.jenson@gmail.com>
2025-05-09 14:38:33 +0100

    updating benchmarks

--- benchmarks/meta_learning/household_electric.py
+++ benchmarks/meta_learning/household_electric.py
@@ -72,9 +72,11 @@ def build_dataloaders(
     num_ctx_min: int = 384,
     num_ctx_max: int = 384,
     num_test: int = 128,
+    pct_train: float = 0.3,
+    pct_test: float = 0.1,
 ):
     B = batch_size
-    train, valid, test = load_data(rng)
+    train, valid, test = load_data(rng, pct_train, pct_test)
     x_train, t_train, f_train = train
     x_valid, t_valid, f_valid = valid
     x_test, t_test, f_test = test
@@ -107,7 +109,7 @@ def build_dataloaders(
     )
 
 
-def load_data(rng: jax.Array):
+def load_data(rng: jax.Array, pct_train: float = 0.3, pct_test: float = 0.1):
     rng_valid, rng_test = random.split(rng)
     path = Path("cache/household_power_consumption.csv")
     try:
@@ -130,10 +132,12 @@ def load_data(rng: jax.Array):
     df = df[x_cols + t_cols + f_cols].dropna()
     df = df.sort_values(by="t").reset_index(drop=True)
     N = df.shape[0]
-    block_size = int(N * 0.1)
+    pct_test, pct_train = 0.1, 0.3
+    block_size = int(N * pct_test)
     df_train, df_test = df[:-block_size], df[-block_size:]
     df_train, df_valid = df_train[:-block_size], df_train[-block_size:]
     df_train, df_valid, df_test = standardize_by_train(df_train, df_valid, df_test)
+    df_train = df_train[df_train["t"] <= pct_train]
     split_xtf = lambda df: [df[c].values for c in [x_cols, t_cols, f_cols]]
     return split_xtf(df_train), split_xtf(df_valid), split_xtf(df_test)
 
