9bf02b4503609fc391593d95f54e7eafb84670ff
Daniel Jenson <daniel.a.jenson@gmail.com>
2025-08-02 14:57:36 +0100

    nits

--- benchmarks/meta_learning/compare_bfa_bsa.sh
+++ benchmarks/meta_learning/compare_bfa_bsa.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 for L in 128 256 512 1024 2048 4096; do
   echo 'BFA vs. BSA:'
-  ./test_biased_flex_attention.py -L $L
-  ./test_biased_scan_attention.py -L $L
+  ./test_biased_flex_attention.py -N 100 -B 32 -H 4 -D 64 -L $L
+  ./test_biased_scan_attention.py -N 100 -B 32 -H 4 -D 64 -L $L
   echo ''
 done
